dd05f7ff1de69cd28aabd31fb3148330e0826d95
Esther Devadas <esmd258@gmail.com>
2024-02-19 01:15:40 +0800

    initial project setup

--- scripts.js
+++ scripts.js
@@ -16,4 +16,39 @@ carouselButton.addEventListener('click', function () {
         faIcon.classList.add('fa-pause');
         carousel.cycle();
     }
-})   
+})
+
+async function fetchWeather() {
+    const apiKey = process.env.OPEN_WEATHER_API_KEY;
+    const city = "Portland";
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        displayWeather(data);
+    } catch (error) {
+        console.error("There was an error!", error);
+    }
+}
+
+fetchWeather();
+
+function displayWeather(data) {
+    const weatherIcon = document.getElementById('weather-icon');
+    const weatherTemp = document.getElementById('weather-temp');
+    const weatherDescription = document.getElementById('weather-description');
+
+    const image = document.createElement('img');
+    const icon = data.weather[0].icon;
+    image.src = `https://openweathermap.org/img/w/${icon}.png`
+    image.alt = "An icon of the current weather."
+    weatherIcon.appendChild(image);
+
+    const temperature = document.createElement('p');
+    temperature.textContent = data.main.temp + '\u00B0'
+    weatherTemp.appendChild(temperature);
+
+    const description = document.createElement('p');
+    description.textContent = data.weather[0].description;
+    weatherDescription.appendChild(description);
+}
