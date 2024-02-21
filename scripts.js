91d5fc38b35c9c3dc15bc7356e34c3cb6e277e15
Esther Devadas <esmd258@gmail.com>
2024-02-21 22:52:09 +0800

    modified scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+/* See my comments below. */
+
 const carousel = new bootstrap.Carousel('#homeCarousel', {
     interval: 5000,
     pause: false
@@ -25,6 +27,8 @@ async function fetchWeather() {
     try {
         const response = await fetch(url);
         const data = await response.json();
+        console.log(data);
+        /* Missing console.log(data); here as part of the testing. */
         displayWeather(data);
     } catch (error) {
         console.error("There was an error!", error);
@@ -44,11 +48,13 @@ function displayWeather(data) {
     image.alt = "An icon of the current weather."
     weatherIcon.appendChild(image);
 
-    const temperature = document.createElement('p');
-    temperature.textContent = data.main.temp + '\u00B0'
+    /* Not sure if this was intentional. The temperature and description elements were setup as spans which are inline elements. You have nested paragragh elements, which are block elements, inside the spans which isn't technically correct. That is why the values are spread across the screen and not next to the other icons. */
+    temperature.textContent = `${Math.round(data.main.temp)}\u00B0F`; /* End with a semicolon. */
     weatherTemp.appendChild(temperature);
 
-    const description = document.createElement('p');
-    description.textContent = data.weather[0].description;
-    weatherDescription.appendChild(description);
+    weatherDescription.textContent = data.weather[0].description;
+
+    /* Could be done this way. */
+    //weatherTemp.textContent = `${Math.round(data.main.temp)}\u00B0F`;
+    //weatherDescription.textContent = data.weather[0].description;
 }
